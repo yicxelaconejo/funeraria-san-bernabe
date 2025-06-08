e6dc1c7c2188468dffdf50ff586ef9f809911870
yicxelaconejo <yixcelaconejo09@gmail.com>
2025-06-08 13:06:01 -0500

    conexion mongo atlas

--- src/db.js
+++ src/db.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 
- export const connectDB = async () => {
-try {
-    await mongoose.connect("mongodb://localhost:27017/funerariasanbernabe");
-    console.log('>>> DB is connected');  
-} catch (error) {
-    console.log(error);
-    
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("🔗 Conectado a MongoDB Atlas");
+  } catch (error) {
+    console.error("Error al conectar a MongoDB Atlas:", error);
+  }
 };
