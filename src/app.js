57df367909df1cdd0eca3e9f528036775ba4df61
XiomaraRivera20 <klxiomy2020@gmail.com>
2025-06-08 01:41:01 -0500

    feat: Add production serving and deploy scripts

--- src/app.js
+++ src/app.js
@@ -1,27 +1,41 @@
+// src/app.js
 import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import cors from "cors"; 
+import path from "path"; 
+import { fileURLToPath } from "url";
+import { dirname } from "path"; 
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 import authRoutes from "./routes/auth.routes.js";
 import headlineRoutes from "./routes/headlines.routes.js";
 import afiliadoRoutes from "./routes/afiliados.routes.js";
-import cors from "cors";
-
 
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
-}))
+}));
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
+
 
 app.use("/api", authRoutes);
 app.use("/api", headlineRoutes);
 app.use("/api", afiliadoRoutes);
 
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+    });
+}
 
 export default app;
