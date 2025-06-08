0a1d3713fe4bca7c8f9bae95ebeb9bd2f2b022a5
yicxelaconejo <yixcelaconejo09@gmail.com>
2025-06-08 11:21:52 -0500

    actualice el app.js

--- src/app.js
+++ src/app.js
@@ -32,9 +32,9 @@ app.use("/api", headlineRoutes);
 app.use("/api", afiliadoRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
     });
 }
 
