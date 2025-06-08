b008997b177a79b1198d116a8fe79214a2e5cdce
yicxelaconejo <yixcelaconejo09@gmail.com>
2025-06-08 12:25:23 -0500

    version 18

--- src/app.js
+++ src/app.js
@@ -32,10 +32,10 @@ app.use("/api", headlineRoutes);
 app.use("/api", afiliadoRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
-    app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
+//     app.use(express.static(path.join(__dirname, '../frontend/dist')));
+//     app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+// });
 }
 
 export default app;
